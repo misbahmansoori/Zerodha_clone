2836ccd587f1d055e60292456715983d43dd4585
Misbah <misbahmansoori14@gmail.com>
2026-02-03 06:03:10 +0530

    Frontend enhanced ready to deploy no error till now

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import HomePage from "./landing page/home/HomePage";
-import AboutPage from "./landing page/about/AboutPage";
-import PricingPage from "./landing page/pricing/PricingPage";
-import ProductsPage from "./landing page/products/ProductsPage";
-import SupportPage from "./landing page/support/SupportPage";
+import HomePage from "./landing-page/home/HomePage";
+import AboutPage from "./landing-page/about/AboutPage";
+import PricingPage from "./landing-page/pricing/PricingPage";
+import ProductsPage from "./landing-page/products/ProductsPage";
+import SupportPage from "./landing-page/support/SupportPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./landing page/Navbar";
-import Footer from "./landing page/Footer";
-import NotFound from "./landing page/NotFound";
+import Navbar from "./landing-page/Navbar";
+import Footer from "./landing-page/Footer";
+import NotFound from "./landing-page/NotFound";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
